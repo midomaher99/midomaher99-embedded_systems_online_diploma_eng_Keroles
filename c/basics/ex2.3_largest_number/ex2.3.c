8947094c21e4c14e212a393a6ac6669d90e1803e
Mohamed Mostafa <mohamed.mostafa.maher.999@gmail.com>
2021-07-18 18:04:26 +0200

    largest number

---
+++ c/basics/ex2.3_largest_number/ex2.3.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+void main()
+{
+	float x, y, z;
+	printf("Enter three numbers: ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%f %f %f", &x, &y, &z);
+	fflush(stdout);
+	if (x > y)
+	{
+		if(x > z)
+			printf("largest number= %.2f", x);
+		else
+			printf("largest number= %.2f", z);
+	}
+	else
+		printf("largest number= %.2f", y);
+}
